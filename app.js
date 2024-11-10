e7a6e0c59af4f6c971bfa0f82dbaed30c081695b
Rajkumarjayampu <rajkumar.jayampu@gmail.com>
2024-11-10 22:17:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Jenkins!');
+});
+
+app.listen(3000, () => console.log('App running on http://localhost:3000'));
